d91f2b4f1526a2994df113139810dcdcdc35b549
caesar <joaovictorcsatu@gmail.com>
2022-03-20 19:27:51 -0300

    Update

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+const authRouter = require("./Routes/authRoutes");
+const jobRouter = require("./Routes/jobRoutes");
+const connectDB = require("./connect");
 const express = require("express");
 const app = express();
-const connectDB = require("./connect");
+const port = 5000;
+
+//JSONPARSING
+app.use(express.json());
+
+//ROUTES
+app.use("/auth", authRouter);
+app.use("/jobs", jobRouter);
+
+const start = async (req, res) => {
+    try {
+        await connectDB;
+        app.listen(() => {
+            `Connected to port ${port}`;
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
