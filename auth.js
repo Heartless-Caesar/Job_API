8cffe8ba0b5fd60c1d2c54f6806ecaba188281af
caesar <joaovictorcsatu@gmail.com>
2022-03-19 22:36:39 -0300

    Updated

--- auth.js
+++ auth.js
@@ -1,15 +1,27 @@
-const JWT = require("jsonwebtoken");
-const { BadRequest } = require("./middleware/BadRequest");
 const { Unauthorized } = require("./middleware/unauthorized");
+const JWT = require("jsonwebtoken");
+require("dotenv").config();
 
-const login = async (req, res) => {
+const authMiddleware = async (req, res) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         throw new Unauthorized("No authorization header");
     }
 
+    const token = authHeader.split(" ")[1];
+
     try {
-        const user = JWT.sign({});
-    } catch (error) {}
+        const user = JWT.verify(token, process.env.JWT_Secret);
+
+        const { _id, username } = user;
+
+        req.user = { _id, username };
+
+        next();
+    } catch (error) {
+        console.log(req.headers);
+        throw new Unauthorized("Something went wrong in the verification...");
+    }
+    next();
 };
