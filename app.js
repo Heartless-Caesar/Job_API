eb21c700ca43168cb902681c82e190e30c8f555f
caesar <joaovictorcsatu@gmail.com>
2022-03-26 23:54:34 -0300

    Auth middleware to view only token associated data

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const errorHandlerMiddleware = require("./middleware/errorHandlerMiddleware");
+const authenticateUser = require("./middleware/authJWT");
 const { authRouter } = require("./Routes/authRoutes");
-const notFound = require("./middleware/notFound");
 const { jobRouter } = require("./Routes/jobRoutes");
+const notFound = require("./middleware/notFound");
 const connectDB = require("./connect");
 const express = require("express");
 require("express-async-errors");
@@ -14,9 +15,12 @@ app.use(express.json());
 //CUSTOM ERROR, VALIDATION ERROR, DUPLICATION ERROR HANDLER
 app.use(errorHandlerMiddleware);
 
-//ROUTES
+//LOGIN AND REGISTER ROUTES
 app.use("/app/auth", authRouter);
-app.use("/app/jobs", jobRouter);
+
+/*APPLIES THE JWT AUTH SO A USER CAN ONLY VIEW
+THE DATA ASSOCIATED TO THEIR TOKEN AND ACCOUNT*/
+app.use("/app/jobs", authenticateUser, jobRouter);
 
 //404 MIDDLEWARE
 app.use(notFound);
--- middleware/authJWT.js
+++ middleware/authJWT.js
@@ -3,6 +3,7 @@ const Unauthorized = require("./unauthorized");
 require("dotenv").config();
 
 const jwtAuth = async (req, res, next) => {
+    //FINDS HEADER WITH TOKEN
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -12,6 +13,16 @@ const jwtAuth = async (req, res, next) => {
     const token = authHeader.split(" ")[1];
 
     try {
-        const paylaod = JWT.verify(token, process.env.JWT_Secret);
-    } catch (error) {}
+        //VERIFIES THE TOKEN ASSOCIATED TO A USER ID
+        const payload = JWT.verify(token, process.env.JWT_Secret);
+
+        //PASSING USER DATA TO A HEADER ONCE LOGIN IS SUCCESSFUL
+        req.user = { id: payload._id, name: payload.username };
+
+        next();
+    } catch (error) {
+        throw new Unauthorized(error.message);
+    }
 };
+
+module.exports = jwtAuth;
